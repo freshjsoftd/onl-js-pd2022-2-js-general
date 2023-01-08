8c20568138f65e9bb47d2f14fcafce87c0c214fc
yan <yan.braverman37@gmail.com>
2023-01-08 14:06:11 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,83 @@
+'use strict';
+let summ = 20;
+let total = 5;
+
+// const result = 80;
+
+let user = 'John';
+
+// console.log('Welcome ' + user + '!');
+// console.log(`Welcome ${5 ** 3}!`);
+
+function getUser(a, b) {
+  return (
+    a + b
+  )
+}
+
+console.log(getUser(summ, total));
+
+const GREEN = '#0F0';
+const PI = Math.PI;
+// console.log(PI);
+
+// console.log('test');
+// ['test1', 'test2', 'test3'].forEach(item => console.log(item));
+/* console.log(1 / -0);
+console.log(NaN ** 0);
+console.log(user);
+let bool = false;
+console.log(bool);
+console.log(total);
+console.log(getUser);
+console.log(typeof null); */
+
+/* let num = '12325';
+console.log(num);
+console.log(Boolean('12'));
+console.log(parseFloat('2'));
+console.log(!!null) */
+// alert('Hi');
+// console.log(confirm('Are you sure you want to'));
+// console.log(+prompt('Are you sure you want to') + 5);
+// console.log('123' !== 123)
+/* let a = 200;
+let b = 300;
+if (a > b) {
+	console.log('It is good');
+} else if (a === b) {
+	console.log('first');
+  } else {
+    console.log(summ);
+  }
+
+a > b 
+? console.log('It is good') 
+: 
+(a === b) ? console.log('first')
+:
+console.log(summ) */
+/* let result = prompt('Enter a number');
+
+switch (result) {
+	case '1':
+  case '2':
+  case '3':
+  case '4':
+		console.log('It is less than 5');
+		break;
+	case '5':
+		console.log('It is equal 5');
+		break;
+	case '6':
+		console.log('It is more than 5');
+		break;
+	case '7':
+		console.log('It is more than 5');
+		break;
+	case '8':
+		console.log('It is more than 5');
+		break;
+	default:
+		console.log('Unknown');
+} */
